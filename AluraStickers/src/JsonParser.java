f666f3b303211fc364b029fc296f8faacfc4485b
Maria <conceicao.martinho.estudo@gmail.com>
2023-03-30 18:36:43 +0100

    Update JsonParser.java

--- AluraStickers/src/JsonParser.java
+++ AluraStickers/src/JsonParser.java
@@ -8,6 +8,32 @@ import java.util.regex.Pattern;
 public class JsonParser {
      private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*");
      private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
+     
+     public List<Map<String, String>> parse(String json) {
+         Matcher matcher = REGEX_ITEMS.matcher(json);
+         if (!matcher.find()) {
+             throw new IllegalArgumentException("Não encontrou items.");
+         }
+ 
+         String[] items = matcher.group(1).split("\\},\\{");
+         List<Map<String, String>> dados = new ArrayList<>();
+         
+         for (String item : items) {
+             Map<String, String> atributosItem = new HashMap<>();
+             Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
+             while (matcherAtributosJson.find()) {
+                 String atributo = matcherAtributosJson.group(1);
+                 String valor = matcherAtributosJson.group(2);
+                 atributosItem.put(atributo, valor);
+             }
+             dados.add(atributosItem);
+         }
+         return dados;
+     }
+
+     /*
+     private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*");
+     private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
 
      public List<Map<String, String>> parse(String json){
           Matcher matcher = REGEX_ITEMS.matcher(json);
@@ -28,5 +54,5 @@ public class JsonParser {
                dados.add(atributosItem);
           }
           return dados;
-     }
+     }*/
 }
